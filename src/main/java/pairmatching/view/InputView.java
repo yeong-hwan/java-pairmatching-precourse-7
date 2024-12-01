ad13e945c9d5247289d6e55152d32428c94435ee
yeong-hwan <jghff700@naver.com>
2024-12-01 17:28:08 +0900

    refactor(view): append condition input

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,6 +1,8 @@
 package pairmatching.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import pairmatching.domain.Condition;
+import pairmatching.utils.ConditionParser;
 import pairmatching.utils.InputValidator;
 
 public class InputView {
@@ -8,5 +10,11 @@ public class InputView {
         String input = Console.readLine();
 
         return InputValidator.validateFeatureSelect(input);
+    }
+
+    public Condition readCondition() {
+        String input = Console.readLine();
+
+        return ConditionParser.parseCondition(input);
     }
 }
